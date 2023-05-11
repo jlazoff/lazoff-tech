850affc08088e273dbec4a6283b78a2364c9b8e4
jlazoff <13966291+jlazoff@users.noreply.github.com>
2023-05-11 13:51:11 -0400

    New Init

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,80 @@
-import Image from 'next/image'
-import { motion } from 'framer-motion'
-import PlatonicAnimation from '../components/PlatonicAnimation'
-import Head from 'next/head'
+import { useState } from 'react';
 
+export default function Index() {
+  const [shapeType, setShapeType] = useState('cube');
+  const [isShapeTouched, setShapeTouched] = useState(false);
+  const [isBackgroundTouched, setBackgroundTouched] = useState(false);
+  const [isCardVisible, setCardVisible] = useState(false);
 
+  const handleShapeClick = () => {
+    setShapeTouched(true);
+  };
 
-export default function HomePage() {
+  const handleBackgroundClick = () => {
+    setBackgroundTouched(true);
+    setCardVisible(true);
+  };
+
+  const handleCardClick = (e) => {
+    // Prevent background click event from being triggered
+    e.stopPropagation();
+  };
+
+  const handleCardClose = () => {
+    setBackgroundTouched(false);
+    setCardVisible(false);
+  };
+
+  const handleShapeTypeChange = () => {
+    // Change the shape type when clicked
+    const nextShapeType =
+      shapeType === 'cube'
+        ? 'tetrahedron'
+        : shapeType === 'tetrahedron'
+        ? 'octahedron'
+        : shapeType === 'octahedron'
+        ? 'dodecahedron'
+        : shapeType === 'dodecahedron'
+        ? 'icosahedron'
+        : 'star-tetrahedron';
+    setShapeType(nextShapeType);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
-      <Head>
-        <title>Luxurious Web Development</title>
-        <meta name="description" content="High-end website development for hotels and luxury products" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div className="flex items-center justify-center flex-grow">
-        <motion.div initial="initial" animate="animate" variants={fadeIn}>
-          <PlatonicAnimation />
-        </motion.div>
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+      onClick={handleBackgroundClick}
+    >
+      <div
+        className={`w-64 h-64 bg-blue-500 rounded-lg transition duration-500 ${
+          isShapeTouched ? 'shape-animation' : ''
+        }`}
+        onClick={handleShapeClick}
+      >
+        <div className={`shape ${shapeType}`}></div>
       </div>
-    </div>
-  )
-}
 
-const fadeIn = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1, transition: { duration: 1 } },
+      {isCardVisible && (
+        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+          <div className="p-4 bg-white rounded-lg shadow-md" onClick={handleCardClick}>
+            <h1 className="text-2xl font-bold">Joshua Lazoff</h1>
+            <p>Email: jlazoff@gmail.com</p>
+            <p>
+              Websites:{' '}
+              <a href="https://joshualazoff.com" target="_blank" rel="noopener noreferrer">
+                joshualazoff.com
+              </a>{' '}
+              and{' '}
+              <a href="https://lazoff.tech" target="_blank" rel="noopener noreferrer">
+                lazoff.tech
+              </a>
+            </p>
+            <button className="px-4 py-2 mt-4 text-white bg-blue-500 rounded" onClick={handleCardClose}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
