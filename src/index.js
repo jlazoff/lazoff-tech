71eefafdbd3211d764bde7d398c20a03c049a9c6
jlazoff <13966291+jlazoff@users.noreply.github.com>
2023-05-23 04:07:19 -0400

    Logo Change

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
       <a href="https://lazofftech.com" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-        Lazoff.Tech
-        <br />
-        - 🥷
+        Lazoff.Tech 🥷
       </a>
-      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>Made May 21, 2024</div>
     </div>
   )
 }
@@ -19,6 +16,7 @@ function Overlay() {
 createRoot(document.getElementById('root')).render(
   <>
     <App />
+    <Overlay />
     <Leva collapsed />
   </>
 )
