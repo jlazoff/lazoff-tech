105f0da84ab56ef7861d44791a4553a568b5857a
jlazoff <13966291+jlazoff@users.noreply.github.com>
2023-11-07 16:54:17 -0500

    Revert "Fixes"
    This reverts commit 2bf3e9d1e6741ffda9fa93e8a8e46bd8d4ebc7b4.

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Lazoff.Tech',
   description: 'Lazoff.Tech Luxury Websites, Mobile Apps, and Custom Software',
-  link: [
-    { rel: 'icon', href: '/favicon.ico' },
-    {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap',
-    },
-  ],
 }
 
 export default function RootLayout({ children }) {
