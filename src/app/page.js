81c666fb51d799d599d5711b62ef2c23d237b077
jlazoff <13966291+jlazoff@users.noreply.github.com>
2023-11-08 11:49:28 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white bg-black">
 
       <style>{`
         body {
@@ -15,32 +15,28 @@ export default function Home() {
         }
       `}</style>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-4 sm:px-20 text-center space-y-6">
-        <img src="/Lazoff.svg" alt="Lazoff.Tech Logo" className="w-40 sm:w-64 my-6" />
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-4 space-y-6 text-center sm:px-20">
+        <img src="/Lazoff.svg" alt="Lazoff.Tech Logo" className="w-40 my-6 sm:w-64" />
         
-        <h1 className="text-3xl sm:text-4xl mb-3 font-bold">
+        <h1 className="mb-3 text-3xl font-bold sm:text-4xl">
           Crafting Digital Experiences
         </h1>
         
-        <h1 className="text-3xl sm:text-4xl mb-3 font-bold">
+        <h1 className="mb-3 text-3xl font-bold sm:text-4xl">
           Software Engineering &amp; Consulting
         </h1>
 
-        <div className="text-left space-y-4">
-          <h3 className="text-md sm:text-lg font-semibold">Our Services</h3>
-          <p>
-            <span className="text-gold-500">• Websites</span>
-            <span className="text-gold-500">• Mobile Apps</span>
-            <span className="text-gold-500">• Custom Software</span>
-          </p>
+        <div className="space-y-4 text-left">
+          <h3 className="font-semibold text-md sm:text-lg">Our Services</h3>
+          <p className="text-gold-500">Websites, Mobile Apps, Custom Software</p>
         </div>
 
-        <div className="flex justify-center sm:justify-start space-x-3 sm:space-x-4">
+        <div className="flex justify-center space-x-3 sm:justify-start sm:space-x-4">
           <a href="https://www.linkedin.com/in/joshua-lazoff/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">
-            <FontAwesomeIcon icon={faLinkedin} className="h-8 w-8 sm:h-10 sm:w-10" alt="LinkedIn Logo" />
+            <FontAwesomeIcon icon={faLinkedin} className="w-8 h-8 sm:h-10 sm:w-10" alt="LinkedIn Logo" />
           </a>
           <a href="mailto:joshua&#64;lazoff&#46;tech" className="text-gold-500 hover:text-gold-700">
-            <FontAwesomeIcon icon={faEnvelope} className="h-8 w-8 sm:h-10 sm:w-10" alt="Email Icon" />
+            <FontAwesomeIcon icon={faEnvelope} className="w-8 h-8 sm:h-10 sm:w-10" alt="Email Icon" />
           </a>
         </div>
       </main>
