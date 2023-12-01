08b83644909993ab3c9dc04982dbb0369d30e99c
Joshua Lazoff <13966291+jlazoff@users.noreply.github.com>
2023-11-30 23:24:29 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,24 +24,19 @@ export default function Home() {
       </div>
             
         <h1 className="mb-3 text-3xl font-bold sm:text-3xl">
-          Software Engineering &amp; Consulting
+          Software Engineering
         </h1>
-
-        <h2 className="mb-2 text-2xl font-bold sm:text-2xl">
-          Elevating your digital presence with innovative technology solutions
-        </h2>
-
         <section className="mb-6">
           <h2 className="my-4 text-2xl underline sm:text-2xl">Our Services</h2>
           <ul className="flex flex-col text-center text-mb-2">
             <li className="mb-2">
               <span className="mr-2">&#9679;</span>
-              <strong>Website & Mobile Applications</strong><br /><span class="text-mb-1">Crafting sophisticated, user-friendly digital experiences.</span>
+              <strong>Website & Mobile Applications</strong><br /><span class="text-mb-1">Crafting user-friendly digital experiences.</span>
               <br />
             </li>
             <li className="mb-2">
               <span className="mr-2">&#9679;</span>
-              <strong>Custom Software Solutions</strong><br /><span class="text-mb-1">Tailoring technology to align with your distinctive business needs.</span>
+              <strong>Custom Software Solutions</strong><br /><span class="text-mb-1">Tailoring technology for your unique needs.</span>
             </li>
           </ul>
         </section>
